5ecbb87372c3ad267473203e7f693d66f51b3b08
nls-jajuko <nls-jajuko@users.noreply.github.com>
2022-09-16 12:15:26 +0300

    fix public url

--- kiinteisto-avoin/kiinteistojaotus-maplibregljs/index.js
+++ kiinteisto-avoin/kiinteistojaotus-maplibregljs/index.js
@@ -4,7 +4,7 @@ import 'maplibre-gl/dist/maplibre-gl.css';
 
 // let's load the style from project
 
-// ! Edit kiinteistojaotus.json and replace any INSERT-YOUR-API-KEY text (two intances) with your own api-key !
+// ! Edit kiinteistojaotus.json and replace any 7cd2ddae-9f2e-481c-99d0-404e7bc7a0b2 text (two intances) with your own api-key !
 // ! Edit kiinteistojaotus.json to apply your own styling to vector tile data !
 
 // boundaries with raster background map
