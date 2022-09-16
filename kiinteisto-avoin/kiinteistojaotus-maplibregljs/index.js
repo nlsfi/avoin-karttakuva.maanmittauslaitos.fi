501646ed1565cbc385fa9191ed682e9a114ed246
nls-jajuko <nls-jajuko@users.noreply.github.com>
2022-09-16 12:05:02 +0300

    alternate style json

--- kiinteisto-avoin/kiinteistojaotus-maplibregljs/index.js
+++ kiinteisto-avoin/kiinteistojaotus-maplibregljs/index.js
@@ -3,9 +3,14 @@ import { Map } from 'maplibre-gl'
 import 'maplibre-gl/dist/maplibre-gl.css';
 
 // let's load the style from project
+
 // ! Edit kiinteistojaotus.json and replace any INSERT-YOUR-API-KEY text (two intances) with your own api-key !
 // ! Edit kiinteistojaotus.json to apply your own styling to vector tile data !
-import style from './kiinteistojaotus.json';
+
+// boundaries with raster background map
+import style from './kiinteistojaotus-taustakartalla.json';
+// only boundaries
+//import style from './kiinteistojaotus.json';
 
 // let's create the map
 const map = new Map({
